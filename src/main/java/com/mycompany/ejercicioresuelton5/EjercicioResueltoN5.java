216cc75672501c4ef16ae185d622445ac83c15ab
sebas <sebas@Kaladin>
2026-02-12 01:00:04 -0500

    Ejercicio Resuelto No5

---
+++ src/main/java/com/mycompany/ejercicioresuelton5/EjercicioResueltoN5.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicioresuelton5;
+
+/**
+ *
+ * @author sebas
+ */
+public class EjercicioResueltoN5 {
+
+    public static void main(String[] args) {
+        double suma,x,y;
+        suma=0;
+        x=20;
+        y=40;
+        suma=suma+x;
+        x=x+Math.pow(y,2);
+        suma=suma+x/y;
+        System.out.println("El valor de x es:"+x);
+        System.out.println("El valor de y es:"+y);
+        System.out.println("El valor de la suma es:"+suma);
+        
+      
+    }
+}
